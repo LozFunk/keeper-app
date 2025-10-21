82d2518b865b0aa4398450083eb3ce6f6ad1ddad
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-10-21 12:28:17 +0200

    created a header, note, and footer components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Header from './Header';
+import Note from './Note';
+
+function App() {  
+  return (
+    <>
+    < Header />
+    < Note />
+    < Footer />
+    </>
+  )
+}
+
+export default App;
